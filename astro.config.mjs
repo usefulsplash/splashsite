d7b87f358697be95d63982135a6b7fe2c0f47ca2
usefulsplash <usefulsplash@proton.me>
2026-04-08 18:39:25 -0400

    upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
+  vite: {
 	plugins: [tailwindcss()],
-    },
+  },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
