5bf38ec5072aa1278673fc328221c58c1d1928cb
usefulsplash <usefulsplash@proton.me>
2026-04-08 21:03:26 -0400

    removed node bc we did not need that shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
 	plugins: [tailwindcss()],
   },
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
